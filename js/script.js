a13eafeb95dbbf6df12623978148d2551e21145f
limon153 <limonkeliy153@gmail.com>
2017-08-06 00:18:51 +0300

    Add channel fetching by Twitch API

--- js/script.js
+++ js/script.js
@@ -1,18 +1,48 @@
 $(function() {
-  
-  var btnChecked = 'all';
 
-  function checkButton() {
-    $('#' + btnChecked).addClass('checked');
+  var btnChecked = 'all';
+  var favouriteStreams = ['c_a_k_e', 'lasqa', 'mehvsgame',
+                          'mob5ter', 'tysegall', 'relaxbeats'];
+
+
+
+    $.ajax({
+     type: 'GET',
+     url: 'https://api.twitch.tv/kraken/users?login=' + favouriteStreams.join(','),
+     headers: {
+       'Accept': 'application/vnd.twitchtv.v5+json',
+       'Client-ID': 'axjhfp777tflhy0yjb5sftsil'
+     },
+     success: function(data) {
+       console.log(data);
+       var streamers = data.users;
+       for (var i = 0; i < streamers.length; i++) {
+        var streamerName = streamers[i].display_name;
+        var streamerLogo = streamers[i].logo;
+        displayStreamInfo(streamerName, streamerLogo);
+        console.log(streamerName);
+        console.log(streamerLogo);
+       }
+
+     }
+  });
+
+  function displayStreamInfo(name, logo) {
+    $('.stream-container').append(
+      '<div class="stream">' +
+        '<img class="stream-logo" src="' + logo + '" alt="stream-logo">' +
+        '<div class="stream-data">' +
+          '<a target="_blank" href="https://twitch.tv/' + name + '" class="streamer">' + name + '</a>' +
+          '<p class="stream-info">Offline</p>' +
+        '</div>' +
+      '</div>'
+    );
   }
 
-
-
   $('.nav-btn').on('click', function(e) {
-
     btnChecked = e.target.id;
     $('.nav-btn').removeClass('checked');
-    checkButton();
+    $('#' + btnChecked).addClass('checked');
   });
 
 });
